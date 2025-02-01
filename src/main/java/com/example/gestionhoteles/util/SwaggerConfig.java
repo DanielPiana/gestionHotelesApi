4588a6ec81484cc3e017e137ffbebc53dc366abf
Pepitoelgringo <kxzdani@gmail.com>
2025-02-01 16:42:14 +0100

    Cambiada configuracion de Swagger para permitir autentificacion.

--- src/main/java/com/example/gestionhoteles/util/SwaggerConfig.java
+++ src/main/java/com/example/gestionhoteles/util/SwaggerConfig.java
@@ -1,20 +1,25 @@
 package com.example.gestionhoteles.util;
 
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
 @Configuration
 public class SwaggerConfig {
+
     @Bean
     public OpenAPI customOpenAPI() {
+
         return new OpenAPI()
-                .components(new Components())
-                .info(new Info().title("HotelesAPI")
-                        .description("API de Hoteles y Habitaciones")
-                        .contact(new Contact().name("juan").email("a@a.com").url("a.com"))
-                        .version("1.0"));
+                .info(new Info().title("JavaInUse Authentication Service"))
+                .addSecurityItem(new SecurityRequirement().addList("JavaInUseSecurityScheme"))
+                .components(new Components().addSecuritySchemes("JavaInUseSecurityScheme", new SecurityScheme()
+                        .name("JavaInUseSecurityScheme").type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
+
     }
 }
